94c9abcb6465f11c785ea59957210b5234e51896
VatsalMargi <vatsalmargi2002@gmail.com>
2025-07-31 20:33:45 +0530

    cloudinary and multer setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"; // fs is a file system that comes with nodejs for managing files
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+
+        //file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response.url);
+
+        // Remove file after successful upload
+        fs.unlink(localFilePath, (err) => {
+            if (err) console.error("Error deleting local file:", err);
+        });
+        
+        return response;
+    } catch (error) {
+        // Remove file if upload fails
+        if (fs.existsSync(localFilePath)) {
+            fs.unlink(localFilePath, (err) => {
+                if (err) console.error("Error deleting failed file:", err);
+            });
+        }
+
+        return null;
+    }
+}
