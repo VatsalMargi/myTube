792c7a525117ee59a620d1051b3cb95a7694ff0a
VatsalMargi <vatsalmargi2002@gmail.com>
2025-08-09 09:54:17 +0530

    Fetching user profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -348,5 +348,79 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const {userName} = req.params
 
-export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar,updateUserCoverImage}
+    if(!userName?.trim()){
+        throw new ApiError(404,"user Not Found")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                userName: userName?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in: [req.user?._id,"subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName: 1,
+                userName: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel not Found")
+    }
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channel[0],"User Channel fetched Succesfully")
+    )
+
+})
+
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar,updateUserCoverImage, getUserChannelProfile}
